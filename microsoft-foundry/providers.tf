4d4752e5e88f34abd3a39887cc74be4608ed591c
Daniel Powley <danielpowley92@outlook.com>
2026-07-23 21:08:31 +0100

    azuread provider added

--- microsoft-foundry/providers.tf
+++ microsoft-foundry/providers.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.81.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.4"
+    }
   }
 }
 
 provider "azurerm" {
+  # Configuration options
+}
+
+provider "azuread" {
   # Configuration options
 }
